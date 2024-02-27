40381b8f4bb6dcf7b2750fb9899a92432ff24159
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2024-02-27 20:12:14 +0530

    Add files via upload

---
+++ Maximum Subarray.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum=INT_MIN,sum=0;
+        for(auto i:nums){
+            sum+=i;
+            maxSum=max(maxSum,sum);
+            if(sum<0)
+                sum=0;
+        }
+        return maxSum;
+    }
+};
+
+
+//Code-2
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum=nums[0],sum=nums[0];
+        for(int i=1;i<nums.size();i++){
+            sum=max(sum+nums[i],nums[i]);
+            maxSum=max(maxSum,sum);
+        }
+        return maxSum;
+    }
+};
+
+/*
+	Given an integer array nums, find the subarray with the largest sum, and return its sum.
+	
+	Example 1:
+	Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+	Output: 6
+	Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+	
+	Example 2:
+	Input: nums = [1]
+	Output: 1
+	Explanation: The subarray [1] has the largest sum 1.
+	
+	Example 3:
+	Input: nums = [5,4,-1,7,8]
+	Output: 23
+	Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+	
+	T.C : O(n)
+*/
